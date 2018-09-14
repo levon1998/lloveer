567ff1bebbc73e42863e01a754873404678049d4
levon1998 <Levon.babayan.a@gmail.com>
2018-09-15 00:16:17 +0400

    push project

---
+++ app/Jobs/SendVerificationCode.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Jobs;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Bus\Dispatchable;
+use Illuminate\Support\Facades\Mail;
+
+class SendVerificationCode implements ShouldQueue
+{
+    use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
+
+    private $user;
+
+    /**
+     * Create a new job instance.
+     *
+     * @param $user
+     */
+    public function __construct($user)
+    {
+        $this->user = $user;
+    }
+
+    /**
+     * Execute the job. Script to send user verification email
+     *
+     * @return void
+     */
+    public function handle()
+    {
+        $subject = "Please verify your email address.";
+        $firstName = $this->user->first_name;
+        $email = $this->user->email;
+
+        Mail::send('email.verify', [
+            'lastName'         => $this->user->last_name,
+            'firstName'        => $this->user->first_name,
+            'verificationCode' => $this->user->token
+        ],
+            function($mail) use ($email, $firstName, $subject){
+                $mail->from(getenv('FROM_EMAIL_ADDRESS'));
+                $mail->to($email);
+                $mail->subject($subject);
+            });
+    }
+}
---
+++ routes/api.php
@@ -0,0 +1,14 @@
+<?php
+
+use Illuminate\Http\Request;
+
+Route::post('register', 'AuthController@register');
+Route::post('login', 'AuthController@login');
+Route::post('recover', 'AuthController@recover');
+
+Route::group(['middleware' => ['jwt.auth']], function() {
+    Route::get('logout', 'AuthController@logout');
+    Route::get('test', function(){
+        return response()->json(['foo'=>'bar']);
+    });
+});
